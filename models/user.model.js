3bb5a2f3619b588931d559c70ec0bc7bdba7535e
Christian <chrisbarroso32@gmail.com>
2026-09-04 12:38:53 -0600

    Correxión de nombre de porpiedad en el schema de UserSchema

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username: {
+    name: {
         type: String,
         required: [true, 'El nombre de usuario es necesario.'],
         trim: true,
