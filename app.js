18a2aa2f3ff04d4e426a1be08add1cd09cd1f409
Christian <chrisbarroso32@gmail.com>
2026-09-04 12:32:10 -0600

    Se desarrollaron los controladores de Autenticación y se configuró Swagger para probar el proyecto

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from "express"; // Uso de Express.js
 import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express";
+import YAML from 'yamljs'
 
 import { PORT } from "./config/env.js";
 
@@ -10,10 +12,13 @@ import connectToMongodb from "./database/mongodb.js";
 import errorMiddleware from "./middleware/error.middleware.js";
 
 const app = express();
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 app.use(express.json()); // Procesa los datos enviados en formato JSON.
 app.use(express.urlencoded({ extended: false })); // Procesa los datos obtenidos de formularios HTML en un formato simple.
 app.use(cookieParser()); // Procesa las cookies que el navegador del cliente envía adjuntas de forma automática en cada petición HTTP.
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
@@ -28,6 +33,7 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, async () => {
     console.log(`La API esta corriendo en http://localhost:${PORT}`);
+    console.log(`Documentación en http://localhost:${PORT}/api-docs`);
     await connectToMongodb();
 })
 
---
+++ controllers/auth.controller.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import User from "../models/user.model.js";
+import {JWT_EXPIRES_IN, JWT_SECRET} from "../config/env.js";
+
+export const signUp = async (req, res, next) => {
+    // Implementación de la lógica de registro de usuario.
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        // Crear un nuevo usuario
+        const { name, email, password } = req.body;
+
+        // Varificar si el usuario existe
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            const error = new Error('User already exists');
+            error.statusCode = 409;
+            throw error;
+        }
+
+        // Hashear la contraseña
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Crear el usuario
+        const newUser = await User.create([{ name, email, password: hashedPassword }], { session });
+
+        const token = jwt.sign({ userId: newUser[0]._id}, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+
+        await session.commitTransaction();
+        session.endSession();
+
+        res.status(201).json({
+            success: true,
+            message: 'User created successfully',
+            data: {
+                token,
+                users: newUser[0],
+            }
+        });
+    } catch (error){
+        await session.abortTransaction();
+        await session.endSession();
+        next(error);
+    }
+};
+
+export const signIn = async (req, res, next) => {
+    // Implementación de la lógica de inicio de sesión.
+};
+
+export const signOut = async (req, res) => {
+    // Implementación de la lógica de cierre de sesión.
+};
