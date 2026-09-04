e254529c0a121d6ebb00f4ac53b69f61678f17fe
Christian <chrisbarroso32@gmail.com>
2026-09-04 10:32:08 -0600

    Se agregó el Error Handler al porycto

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 import express from "express"; // Uso de Express.js
+import cookieParser from "cookie-parser";
+
 import { PORT } from "./config/env.js";
+
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/suscription.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import connectToMongodb from "./database/mongodb.js";
+import errorMiddleware from "./middleware/error.middleware.js";
 
 const app = express();
+
+app.use(express.json()); // Procesa los datos enviados en formato JSON.
+app.use(express.urlencoded({ extended: false })); // Procesa los datos obtenidos de formularios HTML en un formato simple.
+app.use(cookieParser()); // Procesa las cookies que el navegador del cliente envía adjuntas de forma automática en cada petición HTTP.
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/suscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 // Métodos HTTP de la API
 app.get("/", (req, res) => {
---
+++ middleware/error.middleware.js
@@ -0,0 +1,37 @@
+// Crear una subscription => middleware (checar por fecha de renovación) => middleware (verificar por errores) => next => controller
+const errorMiddleware = (err, req, res, next) => {
+    try {
+        let error = { ...err };
+        error.message = err.message;
+
+        console.error(error);
+
+        // Tipo de error
+        // Mongoose bad ObjectId
+        if (error.name === "CastError") {
+            const message = "Resource Not Found.";
+            error = new Error(message);
+            error.statusCode = 404;
+        }
+
+        // Mongoose duplicate key
+        if (err.code === 11000) {
+            const message = "Duplicate field value entered";
+            error = new Error(message);
+            error.statusCode = 400;
+        }
+
+        // Mongoose validation error
+        if (error.name === "ValidationError") {
+            const message = Object.values(err.errors).map(val => val.message);
+            error = new Error(message.join(', '));
+            error.statusCode = 400;
+        }
+
+        res.status(error.statusCode || 500).json({ success: false, message: error.message } || 'Internal Server Error');
+    } catch (error) {
+        next(error);
+    }
+};
+
+export default errorMiddleware;
